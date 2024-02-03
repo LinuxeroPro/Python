e60ad3eaedaa48a50c060b94c209808059b5531a
LinuxeroPro <linuxero776@gmail.com>
2024-02-03 00:11:50 -0400

    siiii1

--- holamundo.py
+++ holamundo.py
@@ -1,4 +1,4 @@
 nombre = input("Inserte su nombre: ")
 edad = int(input("Inserte su edad: "))
 
-print(f"Bienvenido {nombre}, tienes  {edad} años.")
+print(f"Bienvenido {edad}, tienes {nombre} años.")
