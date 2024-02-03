f9f7fe7b29abb02404c86b92fc67997a9d53dff9
LinuxeroPro <linuxero776@gmail.com>
2024-02-02 20:06:10 -0400

    sii

--- srthfadfgarg.py
+++ srthfadfgarg.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 
 root =  ctk.CTk()
-
+button1 = ctk.CTkButton(root, text="1",  width=5,  height=2).pack()
+button2 = ctk.CTkButton(root, text="2",  x=60, y=30, width=5, height=2).pack()
 root.mainloop()
