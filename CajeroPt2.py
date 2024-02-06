17cf9fc43986b5f6e3e3eec33ce5f0ea9f124c90
LinuxeroPro <linuxero776@gmail.com>
2024-02-05 20:44:27 -0400

    siiii7

--- CajeroPt2.py
+++ CajeroPt2.py
@@ -1,6 +1,6 @@
-import customtkinter as ctk
+import sqlite3 as sq
 
-root = ctk.CTk()
-
-
-root.mainloop()
+print("             Bienvenido al cajero del Reservas")
+print("      ¿Qué tipo de transacción desea hacer el día de hoy? \n")
+print(" Depositar dinero en la cuenta \n Retirar dinero de la cuenta \n")
+opcion1=input(":")
